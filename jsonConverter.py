92ee81eee63134c804a819b97c64f867c345afc1
zcatbear <jamieford34@gmail.com>
2016-11-18 18:29:46 -0600

    Allowed for making executable

--- jsonConverter.py
+++ jsonConverter.py
@@ -1,12 +1,11 @@
-#/bin/python27
+#!/usr/local/bin/python
 import json
 from collections import OrderedDict
 import argparse
 
 parser = argparse.ArgumentParser(description="Tranlate a bro log to its json counterpart")
 parser.add_argument('-i', '--inputFile', help='This is the file to be read in', required=True)
-parser.add_argument('-o', '--outputFile', help="This is the file to be spit out, Default is broToJsonOut.json",
-                    default='broToJsonOut.json')
+parser.add_argument('-o', '--outputFile', help="This is the file to be spit out, Default is broToJsonOut.json", default='broToJsonOut.json')
 parser.add_argument('-p', '--printout', action='store_true')
 args = parser.parse_args()
 
